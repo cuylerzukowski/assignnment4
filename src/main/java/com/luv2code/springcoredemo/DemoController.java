435449acefa549ba4080d076c199e45a6daa0877
cuylerzukowski <cuylerzukowski@gmail.com>
2025-12-04 22:38:05 -0600

    Completed Task 3

---
+++ src/main/java/com/luv2code/springcoredemo/DemoController.java
@@ -0,0 +1,36 @@
+package com.luv2code.springcoredemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    // define a private field for the dependency
+    private Coach myCoach;
+
+    // define a constructor for dependency injection
+    @Autowired
+    public DemoController(Coach theCoach) {
+        myCoach = theCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout() {
+        return myCoach.getDailyWorkout();
+    }
+
+    @GetMapping("/basketball")
+    public String getBasketball() {
+        return "Tennis is my favorite basketball.";
+    }
+    
+}
+
+
+
+
+
+
+
